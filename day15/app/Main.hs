4f24ee74e13f13b582843514b21631a27b0a04c8
namanwahi <nmaman@nplan.io>
2021-12-15 20:56:05 +0000

    day 15

--- day15/app/Main.hs
+++ day15/app/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-import Debug.Trace (trace)
+import Debug.Trace (trace, traceShow)
 import Data.Maybe
 import Data.List (sort)
 import Data.Map (Map)
@@ -11,23 +11,22 @@ import Data.PQueue.Prio.Min (MinPQueue)
 import qualified Data.PQueue.Prio.Min as MinPQueue
 
 type Point =  (Int, Int)
-type RiskMap = Map Point Int
+data RiskMap = RiskMap (Map Point Int) Int Int deriving (Show)
 
 -- for dijkstra
--- distance from source
+-- Maps point from source to distance
 type Dist = Map Point Int
--- previous Point
-type Prev = Map Point Point
--- point PQueue
+-- point min priority queue
 type PointPQueue = MinPQueue Point Int
 
--- part 1
 -- make riskMap from 2-d list. Heightmap is a map from a coordinate (x,y) to a height
 makeRiskMap :: [[Int]] -> RiskMap
-makeRiskMap grid = foldl (\acc (x, y) -> Map.insert (x, y) (grid !! y !! x) acc) Map.empty allCoords
+makeRiskMap grid = RiskMap map maxX maxY
     where
+        map = foldl (\acc (x, y) -> Map.insert (x, y) (grid !! y !! x) acc) Map.empty allCoords
+        maxX = (5 * length (head grid)) - 1
+        maxY = (5 * length grid) - 1
         allCoords = [(x, y) | x <- [0..((length . head) grid)-1], y <-[0..(length grid)-1]]
-
 
 -- key a map where you know there the key exists
 sureLookup :: Ord k => k -> Map k a -> a
@@ -35,46 +34,46 @@ sureLookup key map = fromJust $ Map.lookup key map
 
 -- get neighbours of a point from the heightmap
 getNeighbours :: RiskMap -> Point -> [Point]
-getNeighbours riskMap (x, y) = filter (\p -> Map.member p riskMap) [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+getNeighbours (RiskMap _ maxX maxY) (x, y) = filter inRange [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    where
+        inRange (x, y) = x >= 0 && x <= maxX && y >= 0 && y <= maxY
 
-
-updatePriority :: Point -> Int -> PointPQueue -> PointPQueue
-updatePriority point priority pQueue
-    | point `elem` (MinPQueue.keysU pQueue) = MinPQueue.mapWithKey (\p oldPriority -> if p == point then priority else oldPriority) pQueue
-    | otherwise = MinPQueue.insert point priority pQueue
+-- lookup riskt from risk map
+lookupRisk :: Point -> RiskMap -> Int
+lookupRisk (x, y) (RiskMap coordToRisk maxX maxY) = last $ take (xQuot + yQuot + 1) $ iterate incrementRisk $ sureLookup (correspondingXCoord, correspondingYCoord) coordToRisk
+    where
+        incrementRisk n = if n == 9 then 1 else n + 1
+        originalScannedWidth = (maxX + 1) `div` 5
+        originalScannedHeight = (maxY + 1) `div` 5
+        (xQuot, correspondingXCoord) = (x `quotRem` originalScannedWidth)
+        (yQuot, correspondingYCoord) = (y `quotRem` originalScannedHeight)
 
 -- dijkstra path finding algorith,
 dijkstra :: RiskMap -> Point -> Point -> Int
-dijkstra riskMap start target = dijkstra' (MinPQueue.singleton (0, 0) 0) initialDist initialPrev
+dijkstra riskMap@(RiskMap coordToRisk maxX maxY) start target = dijkstra' (MinPQueue.singleton (0, 0) 0) initialDist
     where
-        initialPrev = Map.empty
         initialDist = Map.unions $ (Map.singleton (0, 0) 0) : [Map.singleton p (maxBound::Int) | p <- nonSourceNodes]
-        nonSourceNodes = filter (/=(0, 0)) $ Map.keys riskMap
-        dijkstra' :: PointPQueue -> Dist -> Prev -> Int
-        dijkstra' pQueue dist prev
+        nonSourceNodes = filter (/=(0, 0)) $ [(x, y) | x<-[0..maxX], y<-[0..maxY]]
+        dijkstra' pQueue dist
             | u == target = distToU
-            | otherwise = trace ("after updae:" ++ show (MinPQueue.toList pQueue'')) $ dijkstra' pQueue'' dist' prev'
+            | otherwise = dijkstra' pQueue'' dist'
             where
-                ((u, _), pQueue') = trace ("before pop: " ++ show (MinPQueue.toList pQueue)) (MinPQueue.deleteFindMin pQueue)
+                ((u, _), pQueue') = (MinPQueue.deleteFindMin pQueue)
                 distToU = sureLookup u dist
                 vs = getNeighbours riskMap u
-                (pQueue'', dist', prev') = trace ("after pop: " ++ show (MinPQueue.toList pQueue') ++ " neighbours: " ++ (show vs)) $ foldr updateState (pQueue', dist, prev) vs
-                updateState :: Point -> (PointPQueue, Dist, Prev) -> (PointPQueue, Dist, Prev)
-                updateState v (pq, d, p)
-                    | alt <= (sureLookup v riskMap) = (updatePriority v alt pq, Map.insert v alt d, Map.insert v u p)
-                    | otherwise = (pq, d, p)
+                (pQueue'', dist') = foldr updateState (pQueue', dist) vs
+                updatePriority point priority pQueue
+                    | point `elem` (MinPQueue.keysU pQueue) = MinPQueue.mapWithKey (\p oldPriority -> if p == point then priority else oldPriority) pQueue
+                    | otherwise = MinPQueue.insert point priority pQueue
+                updateState v (pq, d)
+                    | alt < (sureLookup v d) = (updatePriority v alt pq, Map.insert v alt d)
+                    | otherwise = (pq, d)
                     where
-                        alt' = distToU + (sureLookup v riskMap)
-                        alt = trace ("alt: " ++ show alt' ++ " distance to V " ++ show (sureLookup v riskMap)) alt'
-
+                        alt = distToU + (lookupRisk v riskMap)
 
 main :: IO ()
 main = do
     input <- readFile "app/input.txt"
     let grid = map (map digitToInt) $ lines input :: [[Int]]
-    let riskMap = makeRiskMap grid
-    print riskMap
-    print $ getNeighbours riskMap (0,1)
-    let x = 1 :: Int
-    print $ MinPQueue.toList $ updatePriority (1,2) 0 $ updatePriority (2,3) 4 $ MinPQueue.singleton (1,2) x
-    print $ dijkstra riskMap (0, 0) (9, 9)
+    let riskMap@(RiskMap coordToRisk maxX maxY) = makeRiskMap grid
+    print $ dijkstra riskMap (0, 0) (maxX, maxY)
